6fbb26ed0c439d23457cc562c13e31d9e0c2e0d5
Luiz Gustavo <luizgustavo55779@gmail.com>
2019-11-03 10:15:57 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,17 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	//Retorna um tipo da interface porém internamente instancia uma implementação.
 	public static SellerDao creatSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 
 }
